8f26d29ebe33c58bbdab45ccd00e2dbcb2fd524c
seemakaur11 <raiseema248@gmail.com>
2024-04-26 00:35:11 +0530

    5th commit

--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -3,8 +3,8 @@ import Link from "next/link";
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: "Sign Up Page | Free Next.js Template for Startup and SaaS",
-  description: "This is Sign Up Page for Startup Nextjs Template",
+  title: "Sign Up Page",
+  description: "This is Sign Up Page",
   // other metadata
 };
 
