7d8e3113ad1127f771c0c5d5cd73676701dafe0d
seema <87296615+seemakaur11@users.noreply.github.com>
2024-04-30 07:11:16 +0530

    Update index.tsx

--- components/Brands/index.tsx
+++ components/Brands/index.tsx
@@ -50,7 +50,7 @@ const Brands = () => {
               <Swiper
                 spaceBetween={50}
                 slidesPerView={slidesPerView}
-                lazy={true}
+{/*                 lazy={true} */}
                 className="mySwiper"
                 loop={true}
                 autoplay={{ delay: 2000 }}
